273c57f49006cbe0ed64516958581e07f6160802
nomisum <nomisum@googlemail.com>
2018-10-07 22:06:27 +0200

    wutbuerger remove weapons - problems with ctd

--- USER/wutBuerger.hpp
+++ USER/wutBuerger.hpp
@@ -44,20 +44,112 @@ class wutBuerger: RuFlora {
         headgear = "rds_Villager_cap2";
 
         addItemsToUniform[] = {
-
-                LIST_1("rhs_mag_m18_red"),
-                LIST_1("rhs_mag_m18_purple"),
-                LIST_2("rhs_mag_an_m14_th3")
+                LIST_4("HandGrenade")
             };
         addItemsToVest[] = {
-                LIST_2("rhs_mag_an_m14_th3")
+                LIST_2("HandGrenade")
             };
         
     };
     
     class Type {
+        //Rifleman
+        class Soldier_F {
+        };
+
+        //Asst. Autorifleman
+        class soldier_AAR_F: Soldier_F {
+
+        };
+
+        //Asst. Gunner (HMG/GMG)
+        class support_AMG_F: Soldier_F {
+
+        };
+
+        //Asst. Missile Specialist (AA)
+        class soldier_AAA_F: Soldier_F {
+
+        };
+
+        //Asst. Missile Specialist (AT)
+        class soldier_AAT_F: Soldier_F {
+
+        };
+
+        //Autorifleman
+        class soldier_AR_F: Soldier_F {
+
+        };
+
+        //Combat Life Saver
+        class medic_F: Soldier_F {
+
+        };
+
+        //Explosive Specialist
+        class soldier_exp_F: Soldier_F {
+
+        };
+
+        //Grenadier
+        class Soldier_GL_F: Soldier_F {
+
+        };
+
+        //Heavy Gunner
+        class HeavyGunner_F: Soldier_F {
+
+        };
+
+        //Marksman
+        class soldier_M_F: Soldier_F {
+
+        };
+
+        //Missile Specialist (AA)
+        class soldier_AA_F: Soldier_F {
+
+        };
+
+        //Missile Specialist (AT)
+        class soldier_AT_F: Soldier_F {
+
+        };
+
+        //Repair Specialist
+        class soldier_repair_F: Soldier_F {
+
+        };
+
+        //Rifleman (AT)
+        class soldier_LAT_F: Soldier_F {
+
+        };
+
+        //Sniper
+        class Sniper_F: Soldier_F {
+
+        };
+
+        //Squad Leader
+        class Soldier_SL_F: Soldier_F {
+
+        };
+
+        //Spotter
+        class Spotter_F: Soldier_F {
+
+        };
+
+        //Team Leader
+        class Soldier_TL_F: Soldier_F {
+
+        };
+    
+
         //Obermotz
-        class Survivor_F {
+        class Survivor_F: Soldier_F {
             uniform = "rhs_uniform_mvd_izlom";
             secondaryWeapon = "rhs_weap_scorpion";
             vest = "rhs_vest_commander";
@@ -79,5 +171,6 @@ class wutBuerger: RuFlora {
                 LIST_2("rhsgref_20rnd_765x17_vz61")
             };
         };
+        
     };
 };
